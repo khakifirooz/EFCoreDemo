db17198bd0b56b3da790c7020d50f4798622a943
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-13 11:36:10 +0330

    ForeignKeyCorse

---
+++ EFCoreDemo/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCoreDemo.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Teacher Teacher { get; set; }
+}
---
+++ EFCoreDemo/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreDemo.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public int Age { get; set; }
+}
