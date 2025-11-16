3c70facfe4a2e3aab04900af7b4a1b66a1f3c3f6
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-16 12:34:53 +0330

    remove example

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -8,6 +8,14 @@ using var context = new EFCoreDemoContext();
 
 var employee2 = new Employee() { Id = 1, Name = "Mehrshad", Family = "Khaki" };
 context.Employees.Update(employee2);
+
+
+var employee3 = new Employee("Ahmad", "Heidari");
+context.Add(employee3);
+
+var removeEmployee1 = context.Employees.Find(1);
+context.Employees.Remove(removeEmployee1);
+
 context.SaveChanges();
 
 Console.ReadKey();
